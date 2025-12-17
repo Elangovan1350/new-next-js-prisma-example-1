7a46dc3c3e5f3aedd853c6e3ae9a71558ecc3b8e
Elangovan1350 <elangovan2019miss@gmail.com>
2025-12-17 20:24:34 +0530

    change3

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,6 +1,28 @@
 import { prisma } from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  const { id } = await params;
+  const id3 = Number(id);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id3,
+    },
+  });
+
+  return NextResponse.json(
+    {
+      message: "GET user",
+      user,
+    },
+    { status: 200 }
+  );
+};
+
 export const DELETE = async (
   req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
